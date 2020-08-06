cc5050f86eb8be7fe711d774c6e9eeda7ae42944
Josué Monteiro <eaglejosue@gmail.com>
2020-08-05 21:37:36 -0300

    terceiro dia

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,39 +1,65 @@
 import React from 'react';
+import api from '../../services/api';
 
 import iconPurpleHeart from '../../assets/images/icons/purple-heart.svg';
 import iconWhatsapp from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.scss';
 
-export default function TeacherItem() {
+interface TeacherItemProps {
+  classInfo: {
+    "user_id": Number;
+    "name": string;
+    "avatar": string;
+    "title": string;
+    "bio": string;
+    "subject": string;
+    "cost": Number;
+    "whatsapp": string;
+  };
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = props => {
+  function storeConnection() {
+    api.post('connections', { user_id: props.classInfo.user_id });
+  }
+
   return (
     <article className="teacher-item">
       <header>
         <img src="https://avatars2.githubusercontent.com/u/5047991?s=460&u=78793349fb74e7bb9178013f388337a05c4085f3&v=4" alt="Pedro Sanção"/>
         <div>
-          <h3>Pedro Sanção</h3>
-          <span>Matemática</span>
+          <h3>{props.classInfo.name}</h3>
+          <span>{props.classInfo.subject}</span>
         </div>
       </header>
       <p>
-        <strong>Sed velit quam, lacinia non metus sit amet, venenatis bibendum eros. Fusce pharetra interdum luctus. </strong>
+        <strong>{props.classInfo.title}</strong>
       </p>
-      <p>In a condimentum sem. Fusce iaculis sollicitudin iaculis. Maecenas vitae velit sed nisl facilisis euismod ornare non sapien. Praesent venenatis elementum pretium. Aenean tincidunt, lectus in ultrices gravida, justo nisi commodo metus, in venenatis ipsum nisi nec libero. Cras eu vulputate dolor, ac egestas leo. In consectetur lobortis eros, et finibus nunc pulvinar ut.</p>
+      <p>{props.classInfo.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {props.classInfo.cost}</strong>
         </p>
         <div className="buttons-container">
           <button id="favorite-button">
             <img src={iconPurpleHeart} alt="Coração roxo"/>
           </button> 
-          <button id="contact-button">
+          <a
+            href={`https://wa.me/${props.classInfo.whatsapp}`} 
+            id="contact-button" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            onClick={storeConnection}
+          >
             <img src={iconWhatsapp} alt="Whatsapp"/>
             Entrar em contato
-          </button>
+          </a>
         </div>
       </footer>
     </article>
   )
 }
+
+export default TeacherItem;
--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,14 +1,196 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import api from '../../services/api';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+// import SelectSubject from '../../components/SelectSubject';
+// import SelectWeekday from '../../components/SelectWeekday';
+
+import iconWarning from '../../assets/images/icons/warning.svg';
 
 import './styles.scss';
 
-export default function TeacherForm() {
+export default function TeacherForm ({ history }: RouteComponentProps) {
+  const defaultScheduleItem = { weekday: '', timeFrom: '', timeTo: '' };
+  const [scheduleItems, setScheduleItems] = useState([defaultScheduleItem]);
+  const [mayAddSchedule, setMayAddSchedule] = useState(true);
+
+  const [name, setName]         = useState('');
+  const [avatar, setAvatar]     = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [title, setTitle]       = useState('');
+  const [bio, setBio]           = useState('');
+  const [subject, setSubject]   = useState('');
+  const [cost, setCost]         = useState('');
+
+
+  function addScheduleItem() {
+    if (scheduleItems.length >= 6) {
+      setMayAddSchedule(false);
+    }
+    setScheduleItems([...scheduleItems, defaultScheduleItem]);
+  }
+
+  function setScheduleItemValue(itemIndex: Number, field: string, value: string) {
+    setScheduleItems(scheduleItems.map((scheduleItem, index) => {
+      return index === itemIndex ? { ...scheduleItem, [field]: value } : scheduleItem;
+    }));
+  }
+
+  function storeTeacherForm() {
+    api.post('classes', {
+      name,
+      avatar,
+      whatsapp,
+      title,
+      bio,
+      subject,
+      cost,
+      schedule: scheduleItems
+    }).then(({ data }) => {
+      alert('Cadastro realizado com sucesso.');
+      history.push('/');
+    }, error => {
+      console.warn(error);
+      alert('Ocorreu um erro ao salvar, tente novamente.');
+    });
+  }
+
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas.">
-        <p>O primeiro passo é preencher esse formulário de inscrição.</p>
-      </PageHeader>
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas."
+        subtitle="O primeiro passo é preencher esse formulário de inscrição."
+      />
+      <main className="container">
+        <fieldset aria-labelledby="user-legend">
+          <div id="user-legend" className="legend">Seus dados</div>
+          <Input
+            name="name"
+            label="Nome completo"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+          <Input
+            name="avatar"
+            label="Link da sua foto"
+            description="comece com http://"
+            value={avatar}
+            onChange={event => setAvatar(event.target.value)}
+          />
+          <Input
+            name="whatsapp"
+            label="Whatsapp"
+            description="somente números"
+            value={whatsapp}
+            onChange={event => setWhatsapp(event.target.value)}
+          />
+          <Input
+            name="title"
+            label="Título do perfil"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+          <Textarea
+            name="bio"
+            label="Biografia"
+            value={bio}
+            onChange={event => setBio(event.target.value)}
+          />
+        </fieldset>
+        <fieldset aria-labelledby="class-legend">
+          <div id="class-legend" className="legend">Sobre a aula</div>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes' },
+              { value: 'Biologia' },
+              { value: 'Desenho Geométrico' },
+              { value: 'Educação Física' },
+              { value: 'Espanhol' },
+              { value: 'Filosofia' },
+              { value: 'Física' },
+              { value: 'Geografia' },
+              { value: 'História' },
+              { value: 'Inglês' },
+              { value: 'Língua Portuguesa e Literatura' },
+              { value: 'Matemática' },
+              { value: 'Química' },
+              { value: 'Sociologia' }
+            ]}
+            value={subject}
+            onChange={event => setSubject(event.target.value)}
+          />
+          <Input
+            name="cost"
+            label="Custo da sua hora por aula"
+            description="em R$"
+            type="number"
+            value={cost}
+            onChange={event => setCost(event.target.value)}
+          />
+        </fieldset>
+        <fieldset aria-labelledby="schedule-legend">
+          <div id="schedule-legend" className="legend">
+            Horários disponíveis
+            {mayAddSchedule && <button onClick={addScheduleItem}>+ Novo horário</button>}
+          </div>
+          {scheduleItems.map((scheduleItem, index) => {
+            return (
+              <div className="schedule-form" key={index}>
+                <div className="weekday-container">
+                  <Select
+                    name="weekday"
+                    label="Dia da semana"
+                    options={[
+                      { value: '0', label: 'Domingo' },
+                      { value: '1', label: 'Segunda-feira' },
+                      { value: '2', label: 'Terça-feira' },
+                      { value: '3', label: 'Quarta-feira' },
+                      { value: '4', label: 'Quinta-feira' },
+                      { value: '5', label: 'Sexta-feira' },
+                      { value: '6', label: 'Sábado' }
+                    ]}
+                    value={scheduleItem.weekday}
+                    onChange={event => { setScheduleItemValue(index, 'weekday', event.target.value) }}
+                  />
+                </div>
+                <div className="time-from-container">
+                  <Input
+                    name="time-from"
+                    label="Das"
+                    description="hh:mm"
+                    type="time"
+                    value={scheduleItem.timeFrom}
+                    onChange={event => { setScheduleItemValue(index, 'timeFrom', event.target.value) }}
+                  />
+                </div>
+                <div className="time-to-container">
+                  <Input
+                    name="time-to"
+                    label="Até"
+                    description="hh:mm"
+                    type="time"
+                    value={scheduleItem.timeTo}
+                    onChange={event => { setScheduleItemValue(index, 'timeTo', event.target.value) }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </fieldset>
+        <footer>
+          <p>
+            <img src={iconWarning} alt="Ícone de exclamação"/>
+            Importante!<br/>
+            Preencha todos os dados.
+          </p>
+          <button onClick={storeTeacherForm}>Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   )
 }
